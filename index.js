296a9a7ff29219ec324a1c9a7ca89cca181dc8e3
Danil <ferrum268@mail.ru>
2024-04-16 19:44:31 +0500

    task 6

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     const addButton = document.querySelector('.add-button');
+    const forms = document.querySelectorAll(".beverage");
     const form = document.querySelector('form');
     let beverageCount = 1;
 
     addButton.addEventListener('click', function() {
         beverageCount++;
-
-        const newBeverageFieldset = document.createElement('fieldset');
-        newBeverageFieldset.classList.add('beverage');
-
-        const newBeverageHeader = document.createElement('h4');
-        newBeverageHeader.classList.add('beverage-count');
-        newBeverageHeader.textContent = `Напиток №${beverageCount}`;
-
-        const beverageSelect = document.querySelector('.beverage').cloneNode(true);
-        beverageSelect.querySelector('.beverage-count').remove();
-        beverageSelect.querySelector('select').selectedIndex = 0;
+        let newForm = forms[forms.length - 1].cloneNode(true);
+        newForm.querySelector("h4").innerHTML = `Напиток №${beverageCount}`;
+        for (let radio of newForm.querySelectorAll("input[type=radio]")) {
+            radio.name = "milk" + beverageCount;
+        }
 
         const deleteButton = document.createElement('button');
         deleteButton.textContent = '✖️';
         deleteButton.classList.add('delete-button');
         deleteButton.addEventListener('click', function() {
             if (document.querySelectorAll('.beverage').length > 1) {
-                newBeverageFieldset.remove();
+                newForm.remove();
             }
         });
-
-        newBeverageFieldset.appendChild(deleteButton);
-        newBeverageFieldset.appendChild(newBeverageHeader);
-        newBeverageFieldset.appendChild(beverageSelect);
-
-        form.insertBefore(newBeverageFieldset, addButton.parentNode);
+        newForm.appendChild(deleteButton);
+        form.insertBefore(newForm, addButton.parentNode);
     });
 });
 
@@ -41,19 +32,68 @@ document.addEventListener('DOMContentLoaded', function() {
 
     submitButton.addEventListener('click', function (event) {
         event.preventDefault();
+        openModal();
+        createTable();
+    });
+
+    function openModal(){
         modal.style.display = 'block';
+        let num = document.querySelectorAll('fieldset').length;
+        document.querySelector('.status-order').textContent = countBeverages();
+    }
+
+    function countBeverages(){
         let num = document.querySelectorAll('fieldset').length;
         if (num != 1){
             num = (num - 1) / 2 + 1;
         }
         const mod = num % 10;
-        const beverage_count = `Заказ принят! Вы заказали ${num} ${num !== 11 && mod === 1 ?
+        return `Заказ принят! Вы заказали ${num} ${num !== 11 && mod === 1 ?
             'напиток'
             : (num > 20 && (mod === 2 || mod === 3 || mod === 4)) || (num >= 2 && num <= 4) ?
                 'напитка'
                 : 'напитков'}`;
-        document.querySelector('.status-order').textContent = beverage_count;
-    });
+    }
+
+    function createTable(){
+        let dict = {
+            'espresso': 'Эспрессо',
+            'capuccino': 'Капучино',
+            'cacao': 'Какао',
+            'usual' : 'Обычное',
+            'no-fat' : 'Обезжиренное',
+            'soy' : 'Соевое',
+            'coconut' : 'Кокосовое',
+        }
+        const beverages = [];
+        const fields = document.querySelectorAll('.beverage');
+    
+        fields.forEach((field, index) => {
+            const extras = [];
+            field.querySelectorAll('input[type="checkbox"]:checked').forEach(checkbox => {
+                extras.push(checkbox.nextElementSibling.textContent);
+            });
+            beverages.push({
+                beverage: dict[field.querySelector('select').value],
+                milk: dict[field.querySelector('input[type="radio"]:checked').value],
+                extras: extras.join(', ')
+            });
+        });
+    
+        const modalTableBody = document.querySelector('.modal-table tbody');
+        modalTableBody.innerHTML = '';
+    
+        beverages.forEach(beverage => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${beverage.beverage}</td>
+                <td>${beverage.milk}</td>
+                <td>${beverage.extras}</td>
+            `;
+            console.log(row.innerHTML);
+            modalTableBody.appendChild(row);
+        });
+    }
 
     closeModal.addEventListener('click', function () {
         modal.style.display = 'none';
